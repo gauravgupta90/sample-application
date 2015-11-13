628bb6daa594cd0361fa11a5a0b7e24d0c5bf16f
gauravgupta90 <gaurav_bng@hotmail.com>
2015-11-13 14:23:37 +0530

    changed angular structure

--- client/app/student/student.js
+++ client/app/student/student.js
@@ -4,7 +4,15 @@ angular.module('myAppApp')
   .config(function ($routeProvider) {
     $routeProvider
       .when('/student', {
-        templateUrl: 'app/student/student.html',
+        templateUrl: 'app/student/createStudent/student.html',
         controller: 'StudentCtrl'
+      })
+      .when('/listStudent', {
+        templateUrl: 'app/student/listStudent/listStudent.html',
+        controller: 'ListStudentCtrl'
+      })
+      .when('/viewStudent/:id', {
+        templateUrl: 'app/student/viewStudent/viewStudent.html',
+        controller: 'ViewStudentCtrl'
       });
   });
