6d9e43d645dc6151ca0dd4b62080f93f45b8367a
gauravgupta90 <gaurav_bng@hotmail.com>
2015-11-14 19:26:25 +0530

    validation for marks

--- client/app/student/createStudent/student.controller.js
+++ client/app/student/createStudent/student.controller.js
@@ -53,4 +53,30 @@ angular.module('myAppApp')
         }
     };
 })
+.directive("lowerThan", function () {
+    return {
+        require: "ngModel",
+        link: function ($scope, $element, $attrs, ctrl) {
+
+            var validate = function(viewValue) {
+                var comparisonModel = $attrs.lowerThan;
+                
+                if(!viewValue || !comparisonModel){
+                  // It's valid because we have nothing to compare against
+                  ctrl.$setValidity('lowerThan', true);
+                }
+
+                // It's valid if model is lower than the model we're comparing against
+                ctrl.$setValidity('lowerThan', parseInt(viewValue, 10) <= parseInt(comparisonModel, 10) );
+                return viewValue;
+            };
+            ctrl.$parsers.unshift(validate);
+            ctrl.$formatters.push(validate);
+
+            $attrs.$observe('lowerThan', function(comparisonModel){
+                return validate(ctrl.$viewValue);
+            });
+        }
+    };
+});
 
